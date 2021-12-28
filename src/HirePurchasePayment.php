4e49f65c126e1149777a1e55002d6833170f0eb5
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-12-28 17:45:55 +1300

    WIP

--- src/HirePurchasePayment.php
+++ src/HirePurchasePayment.php
@@ -20,6 +20,8 @@ class HirePurchasePayment extends EcommercePayment
 {
     private static $custom_message_for_in_store_payment = '';
 
+    private static $logo = 'https://www.creditcapable.co.nz/ccl.png';
+
     /**
      * Process the In Store payment method.
      *
@@ -39,7 +41,7 @@ class HirePurchasePayment extends EcommercePayment
         return new FieldList(
             new LiteralField(
                 'HirePurchasePayment_BeforeMessage',
-                '<div id="HirePurchasePayment_BeforeMessage"><img src="https://www.creditcapable.co.nz/photowarehouse.png" alt="Online Finance provided by credit capable"></div>'
+                '<div id="HirePurchasePayment_BeforeMessage"><img src="'.$this->Config()->get('logo').'" alt="Online Finance provided by credit capable"></div>'
             ),
             new HiddenField('HirePurchase', 'HirePurchase', 0)
         );
